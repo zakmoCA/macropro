205d668e24713e6194d68d08ec1c355b1ba3559a
zakmoCA <zakeriyaamohamed@gmail.com>
2024-03-15 22:15:19 +1000

    q card and q cycling

---
+++ src/lib/QuestionAnswer.svelte
@@ -0,0 +1,34 @@
+<script>
+  let questionIndex = 0
+  let inputType = ''
+
+const questions = [
+    { label: "Age:", inputType: "text" },
+    { label: "Sex:", inputType: "radio" },
+    { label: "Height (in cm):", inputType: "range" },
+    { label: "Weight (in kg):", inputType: "range" },
+    { label: "Activity Level:", inputType: "radio" },
+    { label: "Goals:", inputType: "radio" }
+  ]
+
+
+  function getQuestion() {
+    const currentQuestion = questions[questionIndex].label
+    console.log(currentQuestion)
+    console.log(questionIndex)
+    questionIndex += 1
+  }
+  
+</script>
+
+<h1>{questions[questionIndex].label}</h1>
+
+<input type={inputType} id="user-input">
+
+<button on:click={getQuestion}>
+  Next Question
+</button>
+
+<style>
+
+</style>
