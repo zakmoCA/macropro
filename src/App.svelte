5c4110ee52737e1a0165b9cbd878011475c2adcd
zakmoCA <zakeriyaamohamed@gmail.com>
2024-03-18 20:55:22 +1000

    update user BMR on UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,27 @@
 <script>
-  import UserInputManager from "./lib/UserInputManager.svelte"
+  import UserInputManager from "./lib/UserInputManager.svelte";
+  import Recommendation from "./lib/Recommendation.svelte";
+
+  let userInput;
+
+  function handleUserInputSubmitted(event) {
+    userInput = event.detail;
+  }
 </script>
 
 <main>
   <div class="question-container">
-    <UserInputManager />
+    <UserInputManager on:userInputSubmitted={handleUserInputSubmitted} />
+    {#if userInput}
+      <Recommendation {userInput} />
+    {/if}
   </div>
 </main>
 
 <style>
-  .question-container {
-    border: 1px solid red;
-    width: 60vw;
-    height: 60vh;
-  }
+    .question-container {
+      border: 1px solid red;
+      width: 60vw;
+      height: 60vh;
+    }
 </style>
